3ad95bb22481648ea7caee4946191fa85f51e3c7
jangtaegun <kamuie627@naver.com>
2019-05-01 18:08:00 +0900

    test

---
+++ komec.marine.equipment/src/main/java/com/dbcore/agent/service/IAgentService2.java
@@ -0,0 +1,17 @@
+package com.dbcore.agent.service;
+
+import java.util.List;
+
+import com.dbcore.agent.vo.AgentInfoVo;
+
+public interface IAgentService2 {
+	
+	public List<Object> selectAgentInfo(AgentInfoVo agentInfoVo)throws Exception;
+	
+	public int getTotalCount(AgentInfoVo agentInfoVo) throws Exception;
+	
+	public void insertAgentInfo(AgentInfoVo agentInfoVo)throws Exception;
+	
+	
+
+}
