2e445d1774fd684af0e7c90148d91a6e94e911d1
kamuie627 <tgjang0627>
2019-10-29 21:15:59 +0900

    j

--- komec.marine.equipment/src/main/java/com/dbcore/agent/Dao/IAgentDao.java
+++ komec.marine.equipment/src/main/java/com/dbcore/agent/Dao/IAgentDao.java
@@ -7,7 +7,7 @@ import com.dbcore.model.CodeVO;
 import com.dbcore.parm.agent.AgentParm;
 
 public interface IAgentDao {
-	
+	//koko
 	public List<Object> selectAgentList(AgentInfoVo agentInfoVo) throws Exception; 	//업체정보조회
 	public int getTotalCount(AgentParm agentParm)throws Exception;					//총카운트
 	public void insertAgentInfo(AgentInfoVo agentInfoVo)throws Exception;
--- komec.marine.equipment/src/main/java/com/dbcore/agent/controller/AgentController.java
+++ komec.marine.equipment/src/main/java/com/dbcore/agent/controller/AgentController.java
@@ -31,7 +31,7 @@ public class AgentController extends BaseController {
 	@Autowired
 	private IAgentService agentService;
 	
-	//9090
+	//90900000
 	@RequestMapping("/agentList.do")
 	public String getAgentList(@ModelAttribute("AgentVo") AgentInfoVo agentInfoVo,ModelMap model, HttpServletRequest request, HttpServletResponse response)throws Exception{
 		
